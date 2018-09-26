4a0fe648f96de9956a5dcae5c06725846b65c353
David Caruso <david@satellogic.com>
2018-09-26 20:52:19 -0300

    added script to setup database, influx daemon and grafana

---
+++ apps/classroom_attention/setup_influx_grafana.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "Setup Influx and grafana"
+systemctl start influxd &
+echo "Starting influxd"
+sleep 4
+echo "Drop old databases"
+influx -execute 'drop database test'
+sleep 2
+echo "Create database"
+influx -execute 'CREATE DATABASE test'
+
+echo "Starting grafana server"
+#service grafana-server start &
